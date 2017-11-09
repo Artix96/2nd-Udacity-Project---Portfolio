0d6c6201db5c231135c860fe5be592196670c31b
Arthur <a.paberzs@yahoo.com>
2017-11-09 21:32:57 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+
+
+CREDITS:
+https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+https://pixabay.com/en/coding-programming-working-macbook-924920/
---
+++ script/script.js
@@ -0,0 +1,22 @@
+function funcResponsive() {
+    var x = document.getElementById("myNav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
